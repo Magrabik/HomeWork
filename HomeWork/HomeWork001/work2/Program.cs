15ffe0241ebb86a050319245fd9cf6f89c745f3b
Alexey <vikulun_a97@mail.ru>
2022-10-18 16:31:47 +0700

    add work2

---
+++ HomeWork/HomeWork001/work2/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Введите первое число");
+string volue1 = Console.ReadLine();
+int a = Convert.ToInt32(volue1);
+
+System.Console.WriteLine("Введите второе число");
+string volue2 = Console.ReadLine();
+int b = Convert.ToInt32(volue2);
+
+System.Console.WriteLine("Введите второе число");
+string volue3 = Console.ReadLine();
+int c = Convert.ToInt32(volue3);
+
+if(a>b<c == a)
+{
+    System.Console.WriteLine(volue1);
+}
+if(b>c<a == b)
+{
+    System.Console.WriteLine(volue2);
+}
+if(c>b<a == c)
+{
+    System.Console.WriteLine(volue3);
+}
