4428ac88764abf01a087421031d32ac319f226bc
Alexey <vikulun_a97@mail.ru>
2022-10-18 17:29:43 +0700

    add work3

---
+++ HomeWork/HomeWork001/work3/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите первое число");
+string vol1 = Console.ReadLine();
+int a = Convert.ToInt32(vol1);
+if (a % 2 == 1) 
+{
+    System.Console.WriteLine("false");
+}
+else{
+    System.Console.WriteLine("true");
+}
