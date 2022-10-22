142fa5c399d60816b74ceebebca60455f0010648
Alexey <vikulun_a97@mail.ru>
2022-10-22 15:13:04 +0700

    add work1

---
+++ HomeWork/HomeWork2/work1/Program.cs
@@ -0,0 +1,24 @@
+﻿int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+
+bool ValidateNumber(int number)
+{
+    if (number < 100 || number >= 1000)
+    {
+        System.Console.WriteLine("Это число не трехзначное");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите трехзначное число: ");
+if (ValidateNumber(number))
+{
+    int lastDigit = (number / 10) % 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} равна {lastDigit}");
+}
