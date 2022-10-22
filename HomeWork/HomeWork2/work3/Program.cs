0fbbf8dfa03fac5b99a6db67341dd416f80839ee
Alexey <vikulun_a97@mail.ru>
2022-10-22 16:44:39 +0700

    add work

---
+++ HomeWork/HomeWork2/work3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3: Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+int Prompt(string message)
+{
+     System.Console.WriteLine(message);
+     int number = Convert.ToInt32(Console.ReadLine());
+     return number;
+}
+
+
+bool ValidateNumber(int number)
+{
+     if (number < 100 || number >= 1000)
+     {
+          System.Console.WriteLine("Это число не трехзначное");
+          return false;
+     }
+     return true;
+}
+
+int number = Prompt("Введите трехзначное число: ");
+if(ValidateNumber(number))
+{
+     int lastDigit = number % 10;
+     System.Console.WriteLine($"Последняя цифра числа {number} равна {lastDigit}");
+}
