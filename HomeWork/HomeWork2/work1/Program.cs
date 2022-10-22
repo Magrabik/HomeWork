acede908769343334f735502102de049116db0d3
Alexey <vikulun_a97@mail.ru>
2022-10-22 16:19:43 +0700

    add work1

--- HomeWork/HomeWork2/work1/Program.cs
+++ HomeWork/HomeWork2/work1/Program.cs
@@ -1,4 +1,14 @@
-﻿int Prompt(string message)
+﻿// Задача 1: Напишите программу, 
+// которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+
+int Prompt(string message)
 {
     System.Console.WriteLine(message);
     int number = Convert.ToInt32(Console.ReadLine());
